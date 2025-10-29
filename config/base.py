435d3ce6c98150480d1b972b9897c229ac4618d7
blaser <vincent.blaser@gmail.com>
2025-10-29 16:47:55 +0100

    Add base config

---
+++ config/base.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+DATA_PATH = Path.cwd()/"data"
+RAW_PBI_PATH = DATA_PATH/"pbi_files"
+UNZIPPED_PATH = DATA_PATH/"unzipped_pbi_folders"
+
+OUTPUT_PATH = Path.cwd()/"output"
